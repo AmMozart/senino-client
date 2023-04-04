68fac8c38a653d4441a8c58e8abeaf36609dc74a
Belyaev Sergey <sergei.beleaev@gmail.com>
2023-04-04 13:38:19 +0300

    test: remove redux wrapper from TextField component

--- src/features/sip/TextField.test.tsx
+++ src/features/sip/TextField.test.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import Wrapper from '../../testUtils';
 import TextField from './TextField';
 
 describe('TextField module', () => {
   const textContent = '123456';
 
   it('should render component', () => {
-    const { getByTestId } = render(<TextField />, { wrapper: Wrapper });
+    const { getByTestId } = render(<TextField />);
     const textField = getByTestId('text-field');
 
     expect(textField).toBeInTheDocument();
   });
 
   it(`should contain text ${textContent}`, () => {
-    const { getByRole } = render(<TextField text={textContent} />, {
-      wrapper: Wrapper,
-    });
+    const { getByRole } = render(<TextField text={textContent} />);
     const input = getByRole('textbox');
 
     expect(input).toHaveValue(textContent);
   });
 
   it('should render delete button', () => {
-    const { getByTestId } = render(<TextField />, {
-      wrapper: Wrapper,
-    });
+    const { getByTestId } = render(<TextField />);
     const button = getByTestId('delete-button');
 
     expect(button).toBeInTheDocument();
@@ -34,10 +29,7 @@ describe('TextField module', () => {
   it('should handle callback on delete character', () => {
     const fn = jest.fn();
     const { getByTestId } = render(
-      <TextField text={textContent} deleteChar={fn} />,
-      {
-        wrapper: Wrapper,
-      }
+      <TextField text={textContent} deleteChar={fn} />
     );
 
     const button = getByTestId('delete-button');
